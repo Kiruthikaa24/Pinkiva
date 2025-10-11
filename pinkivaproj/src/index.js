f8589896345294e2fec82009ed381bfff003a3ca
Kiruthika Anbarasan <159683906+Kiruthikaa24@users.noreply.github.com>
2025-10-11 22:15:43 +0530

    Update index.js

--- pinkivaproj/src/index.js
+++ pinkivaproj/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AllRoutes from './routes/AllRoutes';
 import './assets/styles/App.css'; // ✅ Your main CSS file
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<AllRoutes />);
