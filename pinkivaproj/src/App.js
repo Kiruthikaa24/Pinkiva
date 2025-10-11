659aa86dbb6db2f0ba028ccdecea597257c830fe
Kiruthika Anbarasan <159683906+Kiruthikaa24@users.noreply.github.com>
2025-10-11 23:06:40 +0530

    Update App.js

--- pinkivaproj/src/App.js
+++ pinkivaproj/src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    fetch("https://pinkiva-backend.onrender.com/api/test")
+    fetch("https://pinkivabackend.vercel.app/api/test")
       .then(res => res.json())
       .then(data => setMessage(data.message))
       .catch(err => console.error(err));
