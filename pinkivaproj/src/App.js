c3b2df0dcb855b441303c658a59bebbfbea2ff70
Kiruthika Anbarasan <159683906+Kiruthikaa24@users.noreply.github.com>
2025-10-11 22:16:02 +0530

    Update App.js

--- pinkivaproj/src/App.js
+++ pinkivaproj/src/App.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   const [message, setMessage] = useState("");
