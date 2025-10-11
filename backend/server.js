3007cb40ad0ccbd6b8115c085de3c7c172a8e3b8
Kiruthika Anbarasan <159683906+Kiruthikaa24@users.noreply.github.com>
2025-10-11 23:26:53 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -70,8 +70,8 @@ app.get('*', (req, res) => {
 });
 
 // ---------- START SERVER ----------
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+module.exports = app;
+
 
 
 
